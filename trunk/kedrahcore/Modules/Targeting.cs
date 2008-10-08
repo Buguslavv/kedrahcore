5564349f6ba584bf35e996c7420e2a78410de922
hardeke <hardeke@990648f2-94ca-11dd-8a13-69ae5a795674>
2008-10-08 16:47:06 +0000

    Targeting module started.

---
+++ trunk/kedrahcore/Modules/Targeting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kedrah.Modules
+{
+    public class Targeting : Module
+    {
+        #region Variables/Objects
+
+
+
+        #endregion
+
+        #region Constructor/Destructor
+
+        /// <summary>
+        /// Targeting module constructor.
+        /// </summary>
+        public Targeting(Core core)
+            : base(core)
+        {
+
+            #region Timers
+
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Get/Set Objects
+
+
+
+        #endregion
+
+        #region Module Functions
+
+
+
+        #endregion
+
+        #region Timers
+
+
+
+        #endregion
+    }
+}
